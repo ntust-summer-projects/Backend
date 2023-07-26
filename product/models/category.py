720f7dad2681ea55bdc81c8635f462f5d1d66ba5
B11115049 <B11115049@mail.ntust.edu.tw>
2023-07-26 13:57:07 +0800

    pr-category

--- product/models/category.py
+++ product/models/category.py
@@ -1,26 +1,68 @@
 from django.db import models
-from product.models.abstract import AbstractCategory
+from rest_framework.exceptions import ValidationError
 
-'''
-    create category:
-        CategoryManager.create(categoryType,name, parent = null)
-'''
-class CategoryType(models.TextChoices):
+
+
+class CategoryManager(models.Manager):
+    def __init__(self, categoryType):
+        self.categoryType = categoryType
+        super().__init__()
+    
+    def get_queryset(self,*args,**kwargs):
+        return super().get_queryset(*args,**kwargs).filter(categoryType = self.categoryType)
+        
+class Category(models.Model):
+    class CategoryType(models.TextChoices):
         MATERIAL = "MATERIAL",'material'
         PRODUCT = "PRODUCT", 'product' 
-
-class CategoryManager(models.Manager):   
-    def create(*args, **kwargs):
-        match kwargs.pop('categoryType',None):
-            case CategoryType.MATERIAL:
-                MaterialCategory.objects.create(*args, **kwargs)
-            case CategoryType.PRODUCT:
-                ProductCategory.objects.create(*args, **kwargs)
-
-class MaterialCategory(AbstractCategory):
-    parent = models.ForeignKey(to = 'self', on_delete=models.CASCADE, related_name = 'submcategorys', blank = True, null = True)
+        
+    baseType = CategoryType.MATERIAL
     
-class ProductCategory(AbstractCategory):
-    parent = models.ForeignKey(to = 'self', on_delete=models.CASCADE, related_name = 'subpcategorys', blank = True, null = True)
+    name = models.CharField(max_length = 50, unique = True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name = 'childrens', blank = True, null = True )
+    categoryType = models.CharField(max_length=50, choices= CategoryType.choices, default = CategoryType.MATERIAL, editable = False)
     
+    def __str__(self):
+        if self.parent:
+            return f"{ self.parent }/{ self.name }"
+        else:
+            return self.name
     
+    def save(self, *args, **kwargs):
+        print("Nomal save")
+        if self.pk:
+            p = self.parent
+            while p:
+                if p.pk == self.pk or p.categoryType != self.categoryType:
+                    raise ValidationError("parent error")
+                p = p.parent
+        elif self.parent and self.parent.categoryType != self.categoryType:
+            raise ValidationError("parent error")
+        
+        super().save(*args, **kwargs)
+        
+class ProductCategory(Category):
+    baseType = Category.CategoryType.PRODUCT
+    
+    objects = CategoryManager(baseType)
+    
+    class Meta:
+        proxy = True
+    
+    def save(self, *args, **kwargs):
+        self.categoryType = self.baseType
+        super().save(*args, **kwargs)
+        
+class MaterialCategory(Category):
+    baseType = Category.CategoryType.MATERIAL
+    
+    objects = CategoryManager(baseType)
+    
+    class Meta:
+        proxy = True
+        
+    def save(self, *args, **kwargs):
+        self.categoryType = self.baseType
+        super().save(*args, **kwargs)
+        
+        
