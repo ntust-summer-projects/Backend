ca62794608f8a1a688624603ed300c8dbf6c9d70
B11115049 <B11115049@mail.ntust.edu.tw>
2023-08-15 10:52:41 +0800

    dataGenerator improve

--- general/admin.py
+++ general/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','meta_key','meta_value')
+
+
 admin.site.register(User)
-admin.site.register(Profile)
 admin.site.register(Announcement)
--- test/dataGenerator.py
+++ test/dataGenerator.py
@@ -1,51 +1,36 @@
 from general.models import *
 from product.models import *
 import random
+from faker import Faker
 
-
-
-
+fake = Faker(locale='zh_TW')
 
 def getRandomPersonName():
-    family = ["李", "王", "張", "劉", "陳", "楊", "黃", "趙", "周", "吳", "徐", "孫", "朱", "馬", "胡",
-                  "郭", "林", "何", "高", "梁", "鄭", "羅", "宋", "謝", "唐", "韓", "曹", "許", "鄧", "蕭",
-                  "馮", "曾", "程", "蔡", "彭", "潘", "袁", "於", "董", "餘", "蘇", "葉", "呂", "魏", "蔣",
-                  "田", "杜", "丁", "沈", "姜", "範", "江", "傅", "鐘", "盧", "汪", "戴", "崔", "任", "陸",
-                  "廖", "姚", "方", "金", "邱", "夏", "譚", "韋", "賈", "鄒", "石", "熊", "孟", "秦", "閻",
-                  "薛", "侯", "雷", "白", "龍", "段", "郝", "孔", "邵", "史", "毛", "常", "萬", "顧", "賴",
-                  "武", "康", "賀", "嚴", "尹", "錢", "施", "牛", "洪", "龔"]
-    
-    given = ["世", "中", "仁", "伶", "佩", "佳", "俊", "信", "倫", "偉", "傑", "儀", "元", "冠", "凱",
-                 "君", "哲", "嘉", "國", "士", "如", "娟", "婷", "子", "孟", "宇", "安", "宏", "宗", "宜",
-                 "家", "建", "弘", "強", "彥", "彬", "德", "心", "志", "忠", "怡", "惠", "慧", "慶", "憲",
-                 "成", "政", "敏", "文", "昌", "明", "智", "曉", "柏", "榮", "欣", "正", "民", "永", "淑",
-                 "玉", "玲", "珊", "珍", "珮", "琪", "瑋", "瑜", "瑞", "瑩", "盈", "真", "祥", "秀", "秋",
-                 "穎", "立", "維", "美", "翔", "翰", "聖", "育", "良", "芬", "芳", "英", "菁", "華", "萍",
-                 "蓉", "裕", "豪", "貞", "賢", "郁", "鈴", "銘", "雅", "雯", "霖", "青", "靜", "韻", "鴻", "麗", "龍"]
-    
-    return random.choice(family) + ''.join(random.choices(given, k = random.choice([1,2])))
+    return fake.name()
 
 def getRandomCompanyName():
-    adj = ['成功','勝利','有料','大','小']
-    noun = ['航天','宇航','火箭','建設','博斯']
-    ending = ['有限公司','股份有限公司','公司']
-    
-    return random.choice(adj) + random.choice(noun) + random.choice(ending)
+    return fake.company()
+
+def getRandomEmail():
+    return fake.email()
 
 def getRandomPhone():
-    phone = '09'
-    
-    for i in range(8):
-        phone += str(random.randint(0,9))
-    
-    return phone
+    return fake.phone_number()
 
 def getRandomStr():
-    given = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-             'o','p','q','r','s','t','u','v','w','x','y','z',
-             '0','1','2','3','4','5','6','7','8','9']
-    
-    return ''.join(random.choices(given, k= random.randint(1,30)))
+    return fake.pystr()
+
+def getRandomPassword():
+    return fake.password()
+
+def getRandomAddress():
+    return fake.address()
+
+def getRandomVat():
+    return fake.random_number(digits = 8)
+
+def getRandomCompanyEmail():
+    return fake.company_email()
 
 def getRandomProductName():
     adj = ['毀損', '粗糙', '普通','漂亮','完美', '無暇', '極品', '日本製','中國製']
@@ -53,9 +38,11 @@ def getRandomProductName():
 
     return random.choice(adj) + '的' + random.choice(noun)
 
+
+
 def createNormalUser():
     try:
-        user = User.objects.create(username = getRandomStr(), password = getRandomStr(),  email = getRandomStr() + "@gmail.com", role = User.Role.NORMAL)
+        user = User.objects.create(username = getRandomStr(), password = getRandomPassword(),  email = getRandomEmail(), role = User.Role.NORMAL)
         Profile.objects.update_or_create({'meta_value': getRandomPersonName()}, user = user, meta_key = 'name')
         Profile.objects.update_or_create({'meta_value': getRandomPhone()}, user = user, meta_key = 'phone')
     except Exception as e:
@@ -65,10 +52,12 @@ def createNormalUser():
         
 def createCompany():
     try:
-        company = User.objects.create(username = getRandomStr(), password = getRandomStr(), email = getRandomStr() + "@gmail.com", role = User.Role.COMPANY)
+        company = User.objects.create(username = getRandomStr(), password = getRandomPassword(),  email = getRandomCompanyEmail(), role = User.Role.COMPANY)
         Profile.objects.update_or_create({'meta_value': getRandomCompanyName()}, user = company, meta_key = 'companyName')
         Profile.objects.update_or_create({'meta_value': getRandomPhone()}, user = company, meta_key = 'phone')
         Profile.objects.update_or_create({'meta_value': getRandomPersonName()}, user = company, meta_key = 'chairman')
+        Profile.objects.update_or_create({'meta_value': getRandomAddress()}, user = company, meta_key = 'address')
+        Profile.objects.update_or_create({'meta_value': getRandomVat()}, user = company, meta_key = 'vatNumber')
     except Exception as e:
         print(e)
         createCompany()
@@ -114,4 +103,5 @@ def createProduct():
     except Exception as e:
         print(e)
         createProduct()
+        
         
